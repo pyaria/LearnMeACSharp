99713589f1e2544a94bab3d9a7b1a279d247dfda
mjewkes <mjewkes@gmail.com>
2016-02-22 11:46:51 -0800

    Functional programming tests. Some List cleanup

---
+++ LearnMeACSharp/FunctionalPractice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearnMeACSharp
+{
+    public static class FunctionalPractice
+    {
+        //Add one to each element, and then sum the collection
+        public static int AddOneToAllAndSum(IEnumerable<int> input){
+            throw new NotImplementedException();
+        }
+
+        //Return an array that has two copies of any value that appears only a single time across both lists
+        public static int[] DuplicateUniques(IEnumerable<int> a,IEnumerable<int> b){
+            //You might use SelectMany, and some of these http://msdn.microsoft.com/en-us/library/bb546153.aspx
+            //You probably also need .ToArray() to satisfy the signature
+            throw new System.NotImplementedException();
+        }
+
+        //Returns a function that multiplies its input by the parameter 'a' below.
+        public static Func<int, int> MultiplyBy(int a){
+            // Lambda syntax (input parameters) => expression
+            // Or http://msdn.microsoft.com/en-us/library/bb397687.aspx
+            throw new System.NotImplementedException();
+        }
+    }
+    
+}
